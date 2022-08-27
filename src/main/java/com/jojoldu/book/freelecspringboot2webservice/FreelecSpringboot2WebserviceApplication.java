0e67d4ca4060b4f0c843b29f6ddca6010e240018
je-pa <jieun0502@gmail.com>
2022-08-28 03:50:29 +0900

    HelloControllerTest

--- src/main/java/com/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
@@ -3,7 +3,7 @@ package com.jojoldu.book.freelecspringboot2webservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//이 위치부터 설정을 읽기때문에 항상 최상단!
 public class FreelecSpringboot2WebserviceApplication {
 
     public static void main(String[] args) {
